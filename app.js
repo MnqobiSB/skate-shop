a75dacbdd233b9277b513bf4480d1864108b91f7
MnqobiSB <amandlamm1@gmail.com>
2021-04-18 19:18:15 +0200

    update mongodb connection

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 require('dotenv').config();
 
-const createError 	= require('http-errors');
-const engine        = require('ejs-mate');
-const express 		  = require('express');
-const path 			    = require('path');
-const cookieParser 	= require('cookie-parser');
-const logger 		    = require('morgan');
-const session 		  = require('express-session')
-const passport		  = require('passport');
-const User 			    = require('./models/user');
-const mongoose		  = require('mongoose');
+const createError = require('http-errors');
+const engine = require('ejs-mate');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+const User = require('./models/user');
+const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 // const seedPosts     = require('./seeds');
 // seedPosts();
 
 // require routes
-const indexRouter 	= require('./routes/index');
-const postsRouter 	= require('./routes/posts');
+const indexRouter = require('./routes/index');
+const postsRouter = require('./routes/posts');
 const reviewsRouter = require('./routes/reviews');
 
 const app = express();
 
 // connect to the database
-mongoose.connect('mongodb://localhost:27017/skate-shop', {
-	useNewUrlParser: true, 
+mongoose.connect(process.env.DATABASEURL, {
+	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useCreateIndex: true
 });
@@ -31,7 +31,7 @@ mongoose.connect('mongodb://localhost:27017/skate-shop', {
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log('We\'re Connected to the DB!');
+	console.log("We're Connected to the DB!");
 });
 
 // use ejs-locals for all ejs templates:
@@ -51,11 +51,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
 // configure passport and sessions
-app.use(session({
-  secret: 'skate is life',
-  resave: false,
-  saveUninitialized: true
-}));
+app.use(
+	session({
+		secret: 'skate is life',
+		resave: false,
+		saveUninitialized: true
+	})
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -65,24 +67,24 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // set local variables middleware
-app.use(function(req, res, next) {
-  // req.user = {
-  //   '_id' : '5eb4439c7f321211888ea8b9',
-  //   // '_id' : '5eb4f6dd36cefc1e10a23e49',
-  //   // '_id' : '5eb661922ac90909547cc2f1',
-  //   'username' : 'mnqobi'
-  // }
-  res.locals.currentUser = req.user;
-  // set default page title
-  res.locals.title = 'Skate Shop';
-  // set success flash message
-  res.locals.success = req.session.success || '';
-  delete req.session.success;
-  // set error flash message
-  res.locals.error = req.session.error || '';
-  delete req.session.error;
-  // continue on to next function in middleware chain
-  next();
+app.use(function (req, res, next) {
+	// req.user = {
+	//   '_id' : '5eb4439c7f321211888ea8b9',
+	//   // '_id' : '5eb4f6dd36cefc1e10a23e49',
+	//   // '_id' : '5eb661922ac90909547cc2f1',
+	//   'username' : 'mnqobi'
+	// }
+	res.locals.currentUser = req.user;
+	// set default page title
+	res.locals.title = 'Skate Shop';
+	// set success flash message
+	res.locals.success = req.session.success || '';
+	delete req.session.success;
+	// set error flash message
+	res.locals.error = req.session.error || '';
+	delete req.session.error;
+	// continue on to next function in middleware chain
+	next();
 });
 
 // mount routes
@@ -91,22 +93,22 @@ app.use('/posts', postsRouter);
 app.use('/posts/:id/reviews', reviewsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  // res.locals.message = err.message;
-  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	// res.locals.message = err.message;
+	// res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  // res.status(err.status || 500);
-  // res.render('error');
-  console.log(err);
-  req.session.error = err.message;
-  res.redirect('back');
+	// render the error page
+	// res.status(err.status || 500);
+	// res.render('error');
+	console.log(err);
+	req.session.error = err.message;
+	res.redirect('back');
 });
 
 module.exports = app;
